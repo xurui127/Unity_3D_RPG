ba7c54480eda0dc256a88c8474d9e6ca525c3207
Rui Xu <xurui127@gmail.com>
2023-02-19 20:41:56 -0500

    Start Abstract Anim Script

--- Assets/Script/CharacterBase/Base/AnimController.cs
+++ Assets/Script/CharacterBase/Base/AnimController.cs
@@ -37,4 +37,5 @@ public class AnimController : MonoBehaviour
     }
     #endregion
 
+    
 }
--- Assets/Script/CharacterBase/NPC/Skeleton/SkeletonAnimController.cs
+++ Assets/Script/CharacterBase/NPC/Skeleton/SkeletonAnimController.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class SkeletonAnimController : AnimController
 {
+    private Animator anim;
 
+    protected override void Start()
+    {
+        base.Start();
+        anim = GetComponent<Animator>();
+    }
+
+    public void OnMove()
+    {
+        
+    }
 }
 
